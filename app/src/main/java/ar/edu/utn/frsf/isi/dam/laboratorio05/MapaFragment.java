e9abc77aafd880b7e700350cf4c4937801adb780
lemarillo <leandroamarillo@outlook.com>
2018-11-13 21:32:10 -0300

    Se agrega que el mapa muestre la ubicacion actual

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
@@ -1,14 +1,25 @@
 package ar.edu.utn.frsf.isi.dam.laboratorio05;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -16,7 +27,7 @@ import com.google.android.gms.maps.model.LatLng;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapaFragment extends SupportMapFragment implements OnMapReadyCallback {
+public class MapaFragment extends SupportMapFragment implements OnMapReadyCallback, OnRequestPermissionsResultCallback {
 
     private int tipoMapa;
     private GoogleMap miMapa;
@@ -25,6 +36,7 @@ public class MapaFragment extends SupportMapFragment implements OnMapReadyCallba
     public interface OnMapaListener {
         public void coordenadasSeleccionadas(LatLng c);
     }
+
     public MapaFragment() {
         // Required empty public constructor
     }
@@ -39,8 +51,8 @@ public class MapaFragment extends SupportMapFragment implements OnMapReadyCallba
 
         Bundle argumentos = getArguments();
 
-        if(argumentos != null){
-            tipoMapa = argumentos.getInt("tipo_mapa",0);
+        if (argumentos != null) {
+            tipoMapa = argumentos.getInt("tipo_mapa", 0);
         }
 
         getMapAsync(this);
@@ -49,11 +61,61 @@ public class MapaFragment extends SupportMapFragment implements OnMapReadyCallba
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         miMapa = googleMap;
+
+        actualizarMapa();
     }
+
+    private void actualizarMapa() {
+
+        // Here, thisActivity is the current activity
+        Activity thisActivity = getActivity();
+        if (ActivityCompat.checkSelfPermission(thisActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 9999);
+            return;
+        }else{
+            miMapa.setMyLocationEnabled(true);
+
+        }
+
+    }
+
 
     public void setListener(OnMapaListener listener) {
         this.listener = listener;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    Toast.makeText(getActivity(), "Permission graanted", Toast.LENGTH_SHORT).show();
+                    actualizarMapa();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(getActivity(), "Permission denied", Toast.LENGTH_SHORT).show();
+                    actualizarMapa();
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
 
